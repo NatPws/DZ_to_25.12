42372e79693cc8f3870c7cc4552046ab9600119b
Natalia <natalia.ab.ca@gmail.com>
2023-12-20 22:13:47 +0300

    подготовила рабочее пространство для выполнения ДЗ

---
+++ task2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задайте строку, содержащую латинские буквы в обоих регистрах. 
+//Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+
+
+string text = "Eta sTRoKA soDerZHit latinSKie BuKvi";
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = string.Empty;
+
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if(text[i] == oldValue) 
+        {
+            result = result + $"{newValue}";
+        }
+        else 
+        {
+            result = result + $"{text[i]}";
+        }
+    }
+    return result;
+}
+string newText = Replace(text, "K", "k");
+Console.WriteLine(newText);
